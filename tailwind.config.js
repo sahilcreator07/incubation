3b21ce231979a5798899455a75d046bafee5ec2b
SAMI-THAKUR <2022.sami.thakur@ves.ac.in>
2023-09-11 19:49:52 +0530

    first

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*'],
+  theme: {
+    extend: {
+      colors: {
+        'lightb' : '#AEE2FF',
+        'brightb' : '#00008b',
+        'bg' : '#F5F5F5',
+        'dtext': '0d1117',
+        'text': '#333333',
+      },
+    },
+  },
+  plugins: [],
+}
+
