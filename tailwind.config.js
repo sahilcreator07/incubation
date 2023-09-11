b11c146b5b937d40d62e9db06d43eca886c6e2ca
SAMI-THAKUR <2022.sami.thakur@ves.ac.in>
2023-09-11 21:21:42 +0530

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,24 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'lightb' : '#AEE2FF',
-        'brightb' : '#00008b',
+        'lightb' : '#f5f5f5',
+        'secondary': '#d6d6d6',
+        'brightb' : '#0A2B88',
         'bg' : '#F5F5F5',
         'dtext': '0d1117',
         'text': '#333333',
+      },
+      fontFamily: {
+        'sans': ['ui-sans-serif', 'system-ui'],
+        'serif': ['ui-serif', 'Georgia'],
+        'mono': ['ui-monospace', 'system-ui'],
+        'robo' : ['Roboto Mono','ui-monospace', 'system-ui'],
+        'roboto': ['Roboto Slab', 'ui-serif', 'system-ui'],
+        'name':['Merienda','ui-cursive','system-ui'],
+        'hand':['Patrick Hand','ui-cursive','system-ui'],
+        'play':['ADLaM Display', 'cursive','system-ui'],
+        'poppins':['Poppins','ui-sans-serif','system-ui'],
+        'nav' : ['Acme','ui-sans-serif','system-ui'],
       },
     },
   },
